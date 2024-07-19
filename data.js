d9d46c538a27c60369aaf16198f3df0b08d91b61
paodac <paodac.cbc@gmail.com>
2024-07-18 21:59:44 -0600

    recipes added

--- data.js
+++ data.js
@@ -1,12 +1,131 @@
-cons recipes=[
+const recipes=[
     {
-        name:"",
-        imageUrl:"https://",
+        name:"Cashew Crunch Salad",
+        imageUrl:"https://munchingwithmariyah.com/wp-content/uploads/2023/06/IMG_1636-scaled.jpg/",
         ingredients:[
-            {}
+    { name: 'Tofu', quantity:1, unit:'block',},
+    { name: 'Avocado Oil', quantity:null, unit:null,},
+    { name: 'Salt', quantity:null, unit:null,},
+    { name: 'Thinly Sliced Green Cabage', quantity:3, unit:'cup',},
+    { name: 'Edamame', quantity:1, unit:'cup',},
+    { name: 'Scallions', quantity:3, unit:'pieces',},
+    { name: 'Cilantro', quantity:1/2, unit:'cup',},
+    { name: 'Cashews', quantity:null, unit:null,},
+    { name: 'Toasted Sesame Seeds', quantity:null, unit:null,},
+    { name: 'Tahini', quantity:1/4, unit:'cup',},
+    { name: 'Toasted Sesame Oil', quantity:3, unit:'Tbs',},
+    { name: 'Soy Sauce', quantity:3, unit:'Tbs',},
+    { name: 'Lemon Juice', quantity:3, unit:'Tbs',},
+    { name: 'Honey', quantity:1, unit:'Tbs',},
+    { name: 'Ground Ginger', quantity:1/2, unit:'tsp',},
+    { name: 'Garlic Powder', quantity:1/4, unit:'tsp',},
+    { name: 'Cayenne Pepper', quantity:1/4, unit:'tsp',},
         ],
-        instructions:[],
+        instructions:['Preheat your oven to 450°F and line a baking sheet with parchment paper.', ' Roast the cashews. Dump the cashews onto the prepared baking sheet and bake in the preheated for no more than 5 minutes – toss the nuts halfway through the baking process to ensure they roast evenly. Quick note, cashews (and all nuts) can burn very quickly, so keep a sharp eye on them as they roast. Once the cashews are done, dump them onto your cutting board or a small plate to cool.',' Bake the crispy tofu. Tear the tofu into small bite-size pieces and dump it onto the same parchment-lined baking sheet used for the cashews. Drizzle the tofu with avocado oil and a generous sprinkle of kosher salt, and toss to coat. Bake the tofu in the oven for 20-25 minutes or until it’s golden brown and crisp. Quick note, I prefer tearing the tofu instead of cutting it, as the little craggy nooks from tearing the tofu get super crispy.','Prep the salad. While the tofu bakes, prep cabbage, scallions, edamame and roughly chop the roasted cashews. Add everything into a large serving bowl.','Make the sesame ginger dressing. The last thing to prepare while the tofu bakes is the dressing. Add all the ingredients into a bowl or cup and whisk to combine until silky smooth. The dressing might look broken as you whisk it, but keep whisking and you’ll see it emulsify quickly. Taste and adjust seasonings and salt as needed. This dressing is on the sweeter side, so if you prefer it more savory, reduce the honey by half. ','Make the sesame ginger dressing. The last thing to prepare while the tofu bakes is the dressing. Add all the ingredients into a bowl or cup and whisk to combine until silky smooth. The dressing might look broken as you whisk it, but keep whisking and you’ll see it emulsify quickly. Taste and adjust seasonings and salt as needed. This dressing is on the sweeter side, so if you prefer it more savory, reduce the honey by half. ' ],
     },
+
+
+    {
+        name:"Edamame and Carrot Spaghetti",
+        imageUrl:"https://cookieandkate.com/images/2014/04/sugar-snap-pea-and-carrot-soba-noodles-1-2.jpg",
+        ingredients:[
+            { name: 'Soba Noodles', quantity:6, unit:'oz',},
+            { name: 'Edamame', quantity:2, unit:'cup',},
+            { name: 'Snow Peas', quantity:3, unit:'cup',},
+            { name: 'Carrots', quantity:6, unit:'units',},
+            { name: 'Cilanreo', quantity:1/2, unit:'cup',},
+            { name: 'Sesame Seeds', quantity:1/4, unit:'cup',},
+            { name: 'Soy Sauce', quantity:1/4, unit:'cup',},
+            { name: 'Extra-Virgin Olive Oil', quantity:2, unit:'Tbs',},
+            { name: 'Lime', quantity:1, unit:'unit',},
+            { name: 'Toasted Sesame Oil', quantity:1, unit:'Tbs',},
+            { name: 'Honey', quantity:1, unit:'Tbs',},
+            { name: 'White Miso', quantity:1, unit:'Tbs',},
+            { name: 'Grated Ginger', quantity:2, unit:'tps',},
+            { name: 'Chili Garlic Sauce', quantity:1, unit:'tps',},
+
+        ],
+        instructions:['To prepare the vegetables: Use a chef’s knife to slice the peas in half lengthwise (or just roughly chop them). Slice the carrots into long, thin strips with a julienne peeler, or slice them into ribbons with a vegetable peeler.','To make the sauce: whisk together the ingredients in a small bowl until emulsified. Set aside.','Bring two big pots of water to a boil. In the meantime, toast the sesame seeds: Pour the sesame seeds into a small pan. Toast for about 4 to 5 minutes over medium-low heat, shaking the pan frequently to prevent burning, until the seeds are turning golden and starting to make popping noises.', 'Once the pots of water are boiling: In one pot, cook the soba noodles just until al dente, according to package directions (probably about 5 minutes), then drain and briefly rinse under cool water. Cook the frozen edamame in the other pot until warmed through (about 4 to 6 minutes) but before draining, toss the halved peas into the boiling edamame water and cook for an additional 20 seconds. Drain.', 'Combine the soba noodles, edamame, snap peas and carrots in a large serving bowl. Pour in the dressing and toss with salad servers. Toss in the chopped cilantro and toasted sesame seeds. Serve.' ],
+    },
+
+
+
+
+    {
+        name:"Tempeh with Kale",
+        imageUrl:"https://eatingbirdfood.com/wp-content/uploads/2012/10/tempeh-555-5.jpg",
+        ingredients:[
+            { name: 'Extra-Virgin Olive Oil', quantity:2, unit:'Tbs',},
+            { name: 'Red Pepper Flakes', quantity:1/2, unit:'tsp',},
+            { name: 'Garlic', quantity:6, unit:'cloves',},
+            { name: 'Grated Ginger', quantity:1, unit:'Tbs',},
+            { name: 'Soy Sauce', quantity:3, unit:'Tbs',},
+            { name: 'Maple Syrup', quantity:2, unit:'Tbs',},
+            { name: 'Water', quantity:1/4, unit:'cup',},
+            { name: 'Shiitake Mushrooms', quantity:1, unit:'cup',},
+            { name: 'Tempeh', quantity:8, unit:'oz',},
+            { name: 'Kale', quantity:1, unit:'bunch',},
+            { name: 'Ground Pepper', quantity:1, unit:'tsp',},
+        ],
+        instructions:['In large skillet over low heat, combine the coconut oil, red pepper flakes, garlic and ginger. Cook slowly, taking care not to brown the garlic and stirring occasionally for 5-7 minutes.', 'While the garlic is cooking, whisk together the coconut aminos, maple syrup and water in a small bowl.', 'Increase the temperature under the skillet to medium-high and add the mushrooms and tempeh. Gently stir to get them coated. Add the coconut amino mixture and stir again to coat. Cook for a minute or two, then add the kale. Stir and cover.', 'Cook for about 3 minutes. Uncover, turn the heat up even more, and cook until the tempeh starts to brown. If you need to add more water to prevent sticking, add 1 tablespoon at a time.', 'Remove from heat and stir in the black pepper. Taste, add more pepper if you like, and serve immediately over quinoa or brown rice.' ],
+    },
+    
+
+
+
+    {
+        name:"Hummus",
+        imageUrl:"https://cdn.loveandlemons.com/wp-content/uploads/2019/09/hummus-recip-e.jpg",
+        ingredients:[
+            { name: 'Chickpeas', quantity:1.5, unit:'cup',},
+            { name: 'Tahini', quantity:1/3, unit:'cup',},
+            { name: 'Extra-Virgin Olive Oil', quantity:2, unit:'Tbs',},
+            { name: 'Lemon Juice', quantity:2, unit:'Tbs',},
+            { name: 'Garlic', quantity:1, unit:'clove',},
+            { name: 'Sea Salt', quantity:1/2, unit:'tsp',},
+            { name: 'Water', quantity:5, unit:'Tbs',},
+            { name: 'Paprika', quantity: null, unit: null,},
+        ],
+        instructions:['In a high-speed blender, place the chickpeas, tahini, olive oil, lemon juice, garlic, and salt. Use the blender baton to blend until very smooth, adding water as needed to blend, and/or to reach your desired consistency.', 'Transfer to a serving plate and garnish with paprika, red pepper flakes, and/or fresh parsley. Serve with warm pita and veggies, as desired.'],
+    },
+    
+
+    {
+        name:"Easy Flatbreads",
+        imageUrl:"https:/https://images.immediate.co.uk/production/volatile/sites/2/2024/05/OliveJune245.4.24CoverEffortlessCourgetteHarissaFlatbreads-143-d1b1c2a.gif?quality=90&webp=true&resize=600,545/",
+        ingredients:[
+            { name: 'Extra-Virgin Olive Oil', quantity:2, unit:'Tbs',},
+            { name: 'Rose Harissa', quantity:1, unit:'Tbs',},
+            { name: 'Sherry vinegar', quantity:1, unit:'Tbs',},
+            { name: 'Honey', quantity:2, unit:'tsp',},
+            { name: 'Courgette', quantity:1, unit:'unit',},
+            { name: 'Flatbread', quantity:2, unit:'unit',},
+            { name: 'Ricotta Cheese', quantity:4, unit:'Tbs',},
+            { name: 'Feta Cheese', quantity:50, unit:'g',},
+            { name: 'Pine Nuts', quantity:1, unit:'Tbs',},
+
+        ],
+        instructions:['Whisk together the oil, harissa, vinegar and honey in a bowl. Peel the courgette into long strips using a vegetable peeler, then toss in the dressing and season well. Toast the flatbreads for 1 min until warmed.', 'Spoon the ricotta over the flatbreads, then season. Scatter over the courgette strips, reserving any left-over dressing in the bowl. Crumble over the feta and scatter over the pine nuts, then season. Slide under a hot grill for 5-8 mins or until the courgette wilts and the feta is golden. Alternatively, cook in a hot pizza oven for 2-3 mins or until golden and crisp. Drizzle over any left-over dressing.'],
+    },, ''
+
+    {
+        name:"Vegan Roasted Tomato Soup",
+        imageUrl:"https://munchingwithmariyah.com/wp-content/uploads/2020/09/IMG_9225-1238x1536.jpg",
+        ingredients:[
+            { name: 'Roma Tomatoes', quantity:2, unit:'unit',},
+            { name: 'Onion', quantity:1, unit:'unit',},
+            { name: 'Carrot', quantity:1, unit:'unit',},
+            { name: 'Garlic', quantity:5, unit:'cloves',},
+            { name: 'Water', quantity:3, unit:'cups',},
+            { name: 'Cashews', quantity:1/3, unit:'cups',},
+            { name: 'Chopped Basil', quantity:1/4, unit:'cups',},
+            { name: 'Tomato paste', quantity:1, unit:'Tbs',},
+            { name: 'Salt', quantity:null, unit:null,},
+            { name: 'Pepper', quantity:null, unit:null,},
+        ],
+        instructions:['Start by preheating the oven to 450°F and line a baking tray with parchment paper. ', 'Score the top of the tomatoes across vertically and then across horizontally. This will ensure the tomatoes don’t burst while they’re cooking. Add these onto the baking tray. ', 'Next, chop the carrots and onion into large chunks and add onto the baking tray along with the garlic cloves. Drizzle over the olive oil and bake the veggies in the oven for 45 minutes. The tomatoes should look shriveled, and the onion should have started to burn around the edges — don`t worry it’ll make the soup taste extra yummy!', 'Once the veggies are done, let them cool down for 10 minutes. Once cool, peel off the skin of the tomatoes — it’s easier to peel off the skin when the tomatoes are cooler. ', 'Then, add all of the veggies into a blender, along with the water, cashews, basil, salt and pepper.', 'Blend on low, slowly ramping up the speed to high. Continue to blend on high for 3-4 minutes until the soup is super creamy and thick. Taste and adjust for salt and pepper.',  'Pour the soup into a pot and heat until it’s nice and warm. I love serving this soup with a drizzle of olive and basil on top with a slice of Ezekiel or sourdough bread. ' ],
+    },
+    
 
     {
         name:"",
@@ -16,6 +135,8 @@ cons recipes=[
         ],
         instructions:[],
     },
+    
+    
     
 ]
 
